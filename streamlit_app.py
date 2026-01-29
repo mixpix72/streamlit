1a2e6286aab033d55f4eb3bc059f6a8ae2889a25
mixpix72 <mtmixon@gmail.com>
2026-01-29 12:22:22 -0800

    Added the first slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,19 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
+from datetime import datetime, time 
 
-# st.button("Reset", type="primary")
+st.header('Button demo', divider=True)
+st.button("Reset", type="primary")
 
-# st.button("Just a button", type="secondary")
+st.button("Just a button", type="secondary")
 
-# if st.button('Say hello'):
-# 	st.write('Why hello there')
-# else:
-# 	st.write('Goodbye')
+if st.button('Say hello'):
+	st.write('Why hello there')
+else:
+	st.write('Goodbye')
 
-st.header('st.write demo')
+st.header('st.write demo', divider=True)
 
 st.header('Display Text', divider=True)
 st.write('Hello, *World!* :sunglasses:')
@@ -34,3 +36,10 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+st.header('Slider demo', divider=True)
+
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
