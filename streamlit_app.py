35627a7aaa6cf03882d80fdc20e828f3964dc282
mixpix72 <mtmixon@gmail.com>
2026-01-29 12:30:06 -0800

    Added a few more sliders

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,26 @@ st.header('Slider demo', divider=True)
 
 st.subheader('Slider')
 
-age = st.slider('How old are you?', 0, 130, 25)
+age = st.slider('How old are you?', 0, 100, 25)
 st.write("I'm ", age, 'years old')
+
+st.subheader('Range slider')
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+st.subheader('Range time slider')
+
+st.subheader('Range time slider')
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
+
+st.subheader('Datetime slider')
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
