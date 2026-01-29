22b639241ce750a41c6ce01cc457ceffd7d19db8
mixpix72 <mtmixon@gmail.com>
2026-01-29 10:41:23 -0800

    st write demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import altair as alt
 
-st.button("Reset", type="primary")
+# st.button("Reset", type="primary")
 
-st.button("Just a button", type="secondary")
+# st.button("Just a button", type="secondary")
 
-if st.button('Say hello'):
-	st.write('Why hello there')
-else:
-	st.write('Goodbye')
+# if st.button('Say hello'):
+# 	st.write('Why hello there')
+# else:
+# 	st.write('Goodbye')
+
+st.header('st.write demo')
+st.write('Hello, *World!* :sunglasses:')
+st.write(1234)
