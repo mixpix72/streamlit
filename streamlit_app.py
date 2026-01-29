eb0832364c2e76128a6bd81fb6109a32f081436d
mixpix72 <mtmixon@gmail.com>
2026-01-29 12:10:38 -0800

    Added a plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,11 @@ df = pd.DataFrame({
 	'second column': [10, 20, 30, 40]
 })
 st.write('**Below** is a DataFrame:', df, '**Above** is a dataframe.')
+
+st.header('Display Data Plot', divider=True)
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
